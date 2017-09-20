ae13b06a5217724b34aedca359f45fe655c87a6d
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-09-20 12:23:57 +0900

    merge skeleton-consumer

--- benchmarks/benchmark.hs
+++ benchmarks/benchmark.hs
@@ -2,6 +2,7 @@ import qualified Data.Conduit      as C
 import qualified Data.Conduit.List as C
 import qualified Data.Conduit.Combinators as CC
 import qualified Data.Feeder       as F
+import qualified Data.Predator       as Pd
 import qualified Data.Machine      as M
 import qualified Pipes             as P
 import qualified Pipes.Prelude     as P
@@ -18,8 +19,11 @@ drainP p = runIdentity $ P.runEffect $ P.for (sourceP P.>-> p) P.discard
 drainC :: C.Conduit Int Identity a -> ()
 drainC c = runIdentity $ (sourceC C.$= c) C.$$ C.sinkNull
 
-drainF :: F.Heterotroph Int a Identity () -> ()
-drainF h = fst $ runIdentity $ F.prey F.sinkNull $ sourceF F.@-> h
+drainPd :: Pd.Heterotroph Int a Identity () -> ()
+drainPd h = fst $ runIdentity $ Pd.prey Pd.sinkNull $ sourcePd Pd.@-> h
+
+drainF :: F.Rancher Int a Identity () -> ()
+drainF h = runIdentity $ F.killEater $ snd $ runIdentity $ F.feed sourceF $ h F.>-$ F.sinkNull
 
 value :: Int
 value = 1000
@@ -28,12 +32,16 @@ sourceM = M.enumerateFromTo 1 value
 sourceC = C.enumFromTo 1 value
 sourceP = P.each [1..value]
 
-sourceF :: F.Prey Int Identity ()
+sourcePd :: Pd.Prey Int Identity ()
+sourcePd = Pd.yieldMany [1..value]
+
+sourceF :: F.Feeder Int Identity Identity ()
 sourceF = F.yieldMany [1..value]
 
 main = defaultMain
   [ bgroup "scan"
       [ bench "feeders" $ whnf drainF (F.scan (+) 0)
+      , bench "predators" $ whnf drainPd (Pd.scan (+) 0)
       , bench "machines" $ whnf drainM (M.scan (+) 0)
       , bench "pipes" $ whnf drainP (P.scan (+) 0 id)
       , bench "conduit" $ whnf drainC (CC.scanl (+) 0)
--- src/Data/Feeder.hs
+++ src/Data/Feeder.hs
@@ -8,102 +8,102 @@ import Control.Monad.Trans.State.Strict
 import Data.Bifunctor
 import Data.Functor.Sum
 import Data.Type.Equality
-
--- | Monadic producer
-data Prey s m a = Yield s (Prey s m a)
-  | Lift (m (Prey s m a))
-  | Pure a
-
-instance MonadTrans (Prey s) where
-  lift = Lift . fmap Pure
-
-instance Monad m => Functor (Prey s m) where
-  fmap = liftM
-
-instance Monad m => Applicative (Prey s m) where
-  pure = return
-  (<*>) = ap
-
-instance Monad m => Monad (Prey s m) where
-  return = Pure
-  Pure a >>= k = k a
-  Yield s c >>= k = Yield s $ c >>= k
-  Lift m >>= k = Lift $ fmap (>>=k) m
-  Pure _ >> k = k
-  Yield s c >> k = Yield s $ c >> k
-  Lift m >> k = Lift $ fmap (>>k) m
-
-killPrey :: Monad m => Prey s m a -> m a
-killPrey = \case
-  Pure a -> return a
-  Yield _ k -> killPrey k
-  Lift m -> m >>= killPrey
-
--- | Produce one element.
-yield :: Monad m => s -> Prey s m ()
-yield s = Yield s $ Pure ()
-{-# INLINE yield #-}
-
--- | Produce many values.
-yieldMany :: (Foldable f, Monad m) => f s -> Prey s m ()
-yieldMany = foldr Yield (Pure ())
-{-# INLINE yieldMany #-}
+import Control.Monad.Skeleton
 
 -- | Monadic consumer
-newtype Predator s n m a = Predator { unPredator :: forall x r. Prey s n x -> (a -> Prey s n x -> m r) -> m r }
 
-instance Functor (Predator s n m) where
+data Phage s m x where
+  Await :: Phage s m (Maybe s)
+  Leftover :: s -> Phage s m ()
+  Lift :: m a -> Phage s m a
+
+type Eater s m = Skeleton (Phage s m)
+
+leftover :: s -> Eater s m ()
+leftover = bone . Leftover
+
+await :: Eater s m (Maybe s)
+await = bone Await
+{-# INLINE await #-}
+
+liftP :: m a -> Eater s m a
+liftP = bone . Lift
+{-# INLINE liftP #-}
+
+killEater :: Monad m => Eater s m a -> m a
+killEater m = case debone m of
+  Return a -> return a
+  Await :>>= k -> killEater $ k Nothing
+  Leftover _ :>>= k -> killEater $ k ()
+  Lift m :>>= k -> m >>= killEater . k
+
+-- | Monadic producer
+newtype Feeder s n m a = Feeder { unFeeder :: forall x r. Eater s n x -> (a -> Eater s n x -> m r) -> m r }
+
+instance Functor (Feeder s n m) where
   fmap = liftM
 
-instance Applicative (Predator s n m) where
+instance Applicative (Feeder s n m) where
   pure = return
   (<*>) = ap
   (*>) = (>>)
 
-instance Monad (Predator s n m) where
-  return a = Predator $ \s k -> k a s
-  Predator m >>= k = Predator $ \s cont -> m s $ \a s' -> unPredator (k a) s' cont
-  Predator m >> Predator n = Predator $ \s cont -> m s $ \_ s' -> n s' cont
+instance Monad (Feeder s n m) where
+  return a = Feeder $ \s k -> k a s
+  Feeder m >>= k = Feeder $ \s cont -> m s $ \a s' -> unFeeder (k a) s' cont
+  Feeder m >> Feeder n = Feeder $ \s cont -> m s $ \_ s' -> n s' cont
 
-instance MonadTrans (Predator s n) where
-  lift m = Predator $ \s k -> m >>= \a -> k a s
+instance MonadTrans (Feeder s n) where
+  lift m = Feeder $ \s k -> m >>= \a -> k a s
 
-awaitOn :: forall n m s. (Monad n, Monad m) => (forall x. n x -> m x) -> Predator s n m (Maybe s)
-awaitOn t = Predator go where
-  go :: Prey s n x -> (Maybe s -> Prey s n x -> m r) -> m r
-  go prey cont = case prey of
-    Pure x -> cont Nothing (return x)
-    Yield s k -> cont (Just s) k
-    Lift m -> t m >>= flip go cont
-{-# INLINE awaitOn #-}
+yieldOn :: forall s n m. Monad m => (forall x. n x -> m x) -> s -> Feeder s n m ()
+yieldOn t s = Feeder $ \e cont -> go e >>= cont () where
+  go :: Eater s n x -> m (Eater s n x)
+  go e = case debone e of
+    Return a -> return $ a <$ leftover s
+    Await :>>= k -> return $ k (Just s)
+    Leftover s' :>>= k -> return $ leftover s >> e
+    Lift m :>>= k -> t m >>= go . k
+{-# INLINE yieldOn #-}
 
-await :: Monad m => Predator s m m (Maybe s)
-await = awaitOn id
-{-# INLINE await #-}
+yield :: Monad m => s -> Feeder s m m ()
+yield = yieldOn id
+{-# INLINE yield #-}
 
-sinkNull :: Monad m => Predator s m m ()
+
+yieldMany :: forall s m. Monad m => [s] -> Feeder s m m ()
+yieldMany xs0 = Feeder $ \e cont -> go xs0 e >>= cont () where
+  go :: [s] -> Eater s m x -> m (Eater s m x)
+  go [] e = return e
+  go (x : xs) e = case debone e of
+    Return a -> return $ a <$ mapM_ leftover (x : xs)
+    Await :>>= k -> go xs $ k (Just x)
+    Leftover s' :>>= k -> return $ leftover x >> e
+    Lift m :>>= k -> m >>= go (x : xs) . k
+
+
+sinkNull :: Monad m => Eater s m ()
 sinkNull = await >>= maybe (return ()) (const sinkNull)
 
-sinkList :: Monad m => Predator s m m [s]
+sinkList :: Monad m => Eater s m [s]
 sinkList = await >>= \case
   Nothing -> return []
   Just x -> (x:) <$> sinkList
 
-type Heterotroph a b m = Predator a m (Prey b m)
+type Rancher a b m = Feeder b m (Eater a m)
 
-prey :: Monad m => Predator s n m a -> Prey s n x -> m (a, Prey s n x)
-prey m s = unPredator m s ((return.) . (,))
+feed :: Monad m => Feeder s n m a -> Eater s n x -> m (a, Eater s n x)
+feed m s = unFeeder m s ((return.) . (,))
 
-scan :: Monad m => (b -> a -> b) -> b -> Heterotroph a b m ()
-scan f b = awaitOn lift >>= \case
+scan :: Monad m => (b -> a -> b) -> b -> Rancher a b m ()
+scan f b = lift await >>= \case
   Nothing -> return ()
   Just a -> do
     let !b' = f b a
-    lift $ yield b'
+    yieldOn liftP b'
     scan f b'
 
-(@->) :: Monad m => Prey a m x -> Heterotroph a b m r -> Prey b m r
-p @-> h = do
-  (a, p') <- prey h p
-  lift $ killPrey p'
-  return a
+(>-$) :: Monad m => Rancher a b m x -> Eater b m r -> Eater a m r
+r >-$ e = do
+  (a, e') <- feed r e
+  liftP $ killEater e'
